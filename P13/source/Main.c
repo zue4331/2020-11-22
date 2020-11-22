5023fa1acf12dfcdf76c05cea3f6be62613683f1
zue4331 <71508905+zue4331@users.noreply.github.com>
2020-11-22 22:58:58 +0800

    Add files via upload

---
+++ P13/source/Main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(const int a[][3]);
+
+int main(void)
+{
+	int array1[2][3] = { {1,2,3},{4,5,6} };
+	int array2[2][3] = { 1,2,3,4,5 };
+	int array3[2][3] = { {1,2},{4} };
+
+	printf("Values is array1 by row are¡G\n");
+	printArray(array1);
+
+	printf("Values is array2 by row are¡G\n");
+	printArray(array2);
+
+	printf("Values is array3 by row are¡G\n");
+	printArray(array3);
+	system("pause");
+	return 0;
+}
+
+void printArray(const int a[][3])
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 1; i++)
+	{
+		for(j=0;j<=2;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
